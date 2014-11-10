7fdaf82fb2edd744429a634b8ee2161acc8555ce
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2014-11-10 15:16:49 +0300

    use m2 module

--- m1/src/a/b/c/Foo.java
+++ m1/src/a/b/c/Foo.java
@@ -1,6 +1,5 @@
 package a.b.c;
 
-import a.b.c.Bar;
 import mylibs.aspectj_lib1.myclasses.LibClazz1;
 
 /**
@@ -10,8 +9,9 @@ public class Foo {
 
     public static void main(String[] args) {
         Bar bar = new Bar();
-        LibClazz1 lc1 = new LibClazz1(" test ");
-        String rez = lc1.strMethod1(" foo ")+lc1.getOwnStr1()+bar.foom1()+bar.barm1();
+        Boo boo = new Boo();
+        LibClazz1 lc1 = new LibClazz1("test");
+        String rez = lc1.strMethod1("foo")+" "+lc1.getOwnStr1()+" "+bar.foom1()+" "+bar.barm1()+" "+boo.foom2();
         System.out.println(rez);
     }
 }
--- m2/src/a/b/c/Boo.java
+++ m2/src/a/b/c/Boo.java
@@ -4,4 +4,7 @@ package a.b.c;
  * Created by irina on 11/6/2014.
  */
 public class Boo {
+    public String foom2(){
+        return "boomethod1";
+    }
 }
